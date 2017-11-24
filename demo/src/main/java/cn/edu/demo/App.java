fb740395b34c722dc4da715020b2912f588048ef
Spground <958340585@qq.com>
2017-11-24 11:28:59 +0800

    init maven project

---
+++ demo/src/main/java/cn/edu/demo/App.java
@@ -0,0 +1,13 @@
+package cn.edu.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
